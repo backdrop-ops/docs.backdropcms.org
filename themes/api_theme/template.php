5d37ba16f8bf1392d7fe50f7d84af371869c391b
Nathan Haug <nate@lullabot.com>
2015-01-30 13:29:54 -0800

    Adding typeahead.js autocomplete. Theming the FormAPI table.

--- themes/api_theme/template.php
+++ themes/api_theme/template.php
@@ -1,52 +1,40 @@
 <?php
 
 /**
- * Implements hook_preprocess_maintenance_page().
+ * Implements hook_preprocess_page().
  */
-function bartik_preprocess_maintenance_page(&$variables) {
-  backdrop_add_css(backdrop_get_path('theme', 'bartik') . '/css/maintenance-page.css');
+function api_theme_preprocess_page(&$variables) {
+  backdrop_add_js('core/misc/tableheader.js');
 }
 
 /**
  * Implements hook_preprocess_layout().
  */
-function bartik_preprocess_layout(&$variables) {
+function api_theme_preprocess_layout(&$variables) {
   if ($variables['content']['header']) {
     $variables['content']['header'] = '<div class="l-header-inner">' . $variables['content']['header'] . '</div>';
   }
 }
 
 /**
- * Implements theme_menu_tree().
+ * Implements hook_form_FORM_ID_alter().
  */
-function bartik_menu_tree($variables) {
-  return '<ul class="menu clearfix">' . $variables['tree'] . '</ul>';
+function api_theme_form_search_block_form_alter(&$form, $form_state) {
+  // Add an autocomplete. See api-theme.js.
+  $form['search_block_form']['#attributes']['data-typeahead'] = url('api/search/autocomplete/1');
+  $form['search_block_form']['#attributes']['data-typeahead-selected'] = url('api/backdrop/1/search');
+
+  $form['search_block_form']['#attributes']['placeholder'] = t('Search API and Documentation');
+  $form['search_block_form']['#type'] = 'search';
 }
 
 /**
- * Implements theme_field__field_type().
+ * Implements hook_preprocess_search_results().
  */
-function bartik_field__taxonomy_term_reference($variables) {
-  $output = '';
-
-  // Render the label, if it's not hidden.
-  if (!$variables['label_hidden']) {
-    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
+function api_theme_preprocess_search_results(&$variables) {
+  foreach ($variables['search_results'] as $key => $result) {
+    if (isset($result['result']['type'])) {
+      $variables['search_results'][$key]['info'] = $result['result']['type'];
+    }
   }
-
-  // Render the items.
-  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
-  foreach ($variables['items'] as $delta => $item) {
-    $item_attributes = (isset($variables['item_attributes'][$delta])) ? backdrop_attributes($variables['item_attributes'][$delta]) : '';
-    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $item_attributes . '>' . backdrop_render($item) . '</li>';
-  }
-  $output .= '</ul>';
-
-  // Render the surrounding DIV with appropriate classes and attributes.
-  if (!in_array('clearfix', $variables['classes'])) {
-    $variables['classes'][] = 'clearfix';
-  }
-  $output = '<div class="' . implode(' ', $variables['classes']) . '"' . backdrop_attributes($variables['attributes']) . '>' . $output . '</div>';
-
-  return $output;
 }
