bd3e3b2ee6cf5001d4a436d523b45b279aa2eaf9
Jen Lampton <jen@jeneration.com>
2018-02-22 16:01:06 -0800

    Core update for API site. Backdrop 1.9.2.

--- www/core/misc/backdrop.js
+++ www/core/misc/backdrop.js
@@ -17,7 +17,16 @@ if (Backdrop.settings.drupalCompatibility) {
   window.Drupal = Backdrop;
 }
 
-/**
+// Pre-filter Ajax requests to guard against XSS attacks.
+// This is similar to the fix that is built in to jQuery 3.0 and higher.
+// See https://github.com/jquery/jquery/issues/2432
+$.ajaxPrefilter(function (s) {
+  if (s.crossDomain) {
+    s.contents.script = false;
+  }
+});
+
+  /**
  * Attach all registered behaviors to a page element.
  *
  * Behaviors are event-triggered actions that attach to page elements, enhancing
@@ -130,6 +139,7 @@ Backdrop.detachBehaviors = function (context, settings, trigger) {
 Backdrop.checkPlain = function (str) {
   str = str.toString()
     .replace(/&/g, '&amp;')
+    .replace(/'/g, '&#39;')
     .replace(/"/g, '&quot;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;');
@@ -157,24 +167,75 @@ Backdrop.formatString = function(str, args) {
   // Transform arguments before inserting them.
   for (var key in args) {
     if (args.hasOwnProperty(key)) {
-       switch (key.charAt(0)) {
-         // Escaped only.
-         case '@':
-           args[key] = Backdrop.checkPlain(args[key]);
-        break;
+      switch (key.charAt(0)) {
+        // Escaped only.
+        case '@':
+          args[key] = Backdrop.checkPlain(args[key]);
+          break;
         // Pass-through.
-         case '!':
-           break;
-         // Escaped and placeholder.
-         case '%':
-         default:
-           args[key] = Backdrop.theme('placeholder', args[key]);
-           break;
-       }
-       str = str.replace(key, args[key]);
+        case '!':
+          break;
+        // Escaped and placeholder.
+        case '%':
+          default:
+          args[key] = Backdrop.theme('placeholder', args[key]);
+          break;
+      }
     }
   }
-  return str;
+  return Backdrop.stringReplace(str, args, null);
+};
+
+/**
+ * Replace substring.
+ *
+ * The longest keys will be tried first. Once a substring has been replaced,
+ * its new value will not be searched again.
+ *
+ * @param {String} str
+ *   A string with placeholders.
+ * @param {Object} args
+ *   Key-value pairs.
+ * @param {Array|null} keys
+ *   Array of keys from the "args".  Internal use only.
+ *
+ * @return {String}
+ *   Returns the replaced string.
+ */
+Backdrop.stringReplace = function (str, args, keys) {
+  if (str.length === 0) {
+    return str;
+  }
+
+  // If the array of keys is not passed then collect the keys from the args.
+  if (!$.isArray(keys)) {
+    keys = [];
+    for (var k in args) {
+      if (args.hasOwnProperty(k)) {
+        keys.push(k);
+      }
+    }
+
+    // Order the keys by the character length. The shortest one is the first.
+    keys.sort(function (a, b) { return a.length - b.length; });
+  }
+
+  if (keys.length === 0) {
+    return str;
+  }
+
+  // Take next longest one from the end.
+  var key = keys.pop();
+  var fragments = str.split(key);
+
+  if (keys.length) {
+    for (var i = 0; i < fragments.length; i++) {
+      // Process each fragment with a copy of remaining keys.
+      fragments[i] = Backdrop.stringReplace(fragments[i], args, keys.slice(0));
+    }
+  }
+
+  return fragments.join(args[key]);
 };
 
 /**
@@ -401,6 +462,29 @@ Backdrop.getSelection = function (element) {
     return { 'start': start, 'end': end };
   }
   return { 'start': element.selectionStart, 'end': element.selectionEnd };
+};
+
+/**
+ * Add a global variable which determines if the window is being unloaded.
+ *
+ * This is primarily used by Backdrop.displayAjaxError().
+ */
+Backdrop.beforeUnloadCalled = false;
+$(window).bind('beforeunload pagehide', function () {
+    Backdrop.beforeUnloadCalled = true;
+});
+
+/**
+ * Displays a JavaScript error from an Ajax response when appropriate to do so.
+ */
+Backdrop.displayAjaxError = function (message) {
+  // Skip displaying the message if the user deliberately aborted (for example,
+  // by reloading the page or navigating to a different page) while the Ajax
+  // request was still ongoing. See, for example, the discussion at
+  // http://stackoverflow.com/questions/699941/handle-ajax-error-when-a-user-clicks-refresh.
+  if (!Backdrop.beforeUnloadCalled) {
+    alert(message);
+  }
 };
 
 /**
