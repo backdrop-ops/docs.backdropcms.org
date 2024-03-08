600f5b96c5abc6d4835442f627208a1a9b416538
Jen Lampton <jen@jenerationweb.com>
2024-03-07 19:10:44 -0800

    Update for paragraphs.

--- www/modules/contrib/paragraphs/js/paragraphs_modal_admin.js
+++ www/modules/contrib/paragraphs/js/paragraphs_modal_admin.js
@@ -1,6 +1,16 @@
-(function($, Backdrop) {
-    Backdrop.ajax.prototype.commands.paragraphs_modal_admin_message_hide = function(ajax, response, status) {
-        $('.paragraphs-modal-admin-message').delay(1000).fadeOut(800);
-        //alert(response.value1);
+(function ($, Backdrop) {
+    Backdrop.ajax.prototype.commands.paragraphs_modal_admin_message_hide = function (ajax, response, status) {
+        $('.paragraphs-modal-admin-message').delay(1000).fadeOut(800, function () {
+            $(this).remove();
+        });
+    }
+
+    Backdrop.ajax.prototype.commands.paragraphs_modal_admin_refresh = function (ajax, response, status) {
+        $(response.selector).addClass('paragraphs-item-stale');
+        $('.paragraphs-item-stale').removeClass(response.selector);
+        $('.paragraphs-item-stale').after(response.new_element);
+        $(response.selector).addClass('paragraphs-item-modal-admin');
+        $(response.selector).prepend($('.paragraphs-item-stale > .dropbutton-wrapper'));
+        $('.paragraphs-item-stale').remove();
     }
 })(jQuery, Backdrop);
