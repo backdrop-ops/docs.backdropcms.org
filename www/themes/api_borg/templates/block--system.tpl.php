67f720c42d2ca5867f06eb5d4f67efefe5cf8ba6
Docs Site <docs@backdropcms.org>
2025-03-11 13:17:36 -0700

    Add title to system block.

---
+++ www/themes/api_borg/templates/block--system.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Remove the title from all menu blocks.
+ */
+?>
+<div class="<?php print implode(' ', $classes); ?>">
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h2 class="block-title"><?php print $title; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php print render($content); ?>
+</div>
